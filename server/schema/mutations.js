feeb17f2db432b5b25bf7ec183d19bc34c96af14
Sean Hong <iamseanhong@gmail.com>
2018-06-20 20:40:28 -0700

    Using AuthService for signup mutation

---
+++ server/schema/mutations.js
@@ -0,0 +1,23 @@
+const { GraphQLObjectType, GraphQLString } = require('graphql');
+
+const UserType = require('./types/user_type');
+
+const AuthService = require('../services/auth');
+
+const mutations = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    signup: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString },
+      },
+      resolve(parentValue, args, req) {
+        return AuthService.signup({ email: args.email, password: args.password, req });
+      },
+    },
+  },
+});
+
+module.exports = mutations;
