b2fa0e01b3446461d1325ac87b7a19e015d7a2c4
Sean Hong <iamseanhong@gmail.com>
2018-06-20 20:15:52 -0700

    Start from step-grid starter repo

--- server/server.js
+++ server/server.js
@@ -12,10 +12,10 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://localhost/step-grid-graphql-auth';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
-mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
 
 // Connect to the mongoDB instance and log a message
 // on success or failure
