672e8c1fff75db482ed7deec61ee7c88c381cef3
Sean Hong <iamseanhong@gmail.com>
2018-06-29 21:57:54 -0700

    Directly clicking test-page does not kick me out to login

--- client/components/requireAuth.js
+++ client/components/requireAuth.js
@@ -6,38 +6,31 @@ import query_current_user from '../queries/query_current_user';
 
 export default WrappedComponent => {
   class RequireAuth extends Component {
-    componentDidMount() {
-      // console.log('aaaa');
-    }
-
-    // componentWillUpdate() {
-    //   console.log('this.props: ', this.props);
+    // componentDidMount() {
+    //   // const { user, loading } = this.props.data;
+    //   // if (!user && !loading) {
+    //   //   this.props.history.push('/signin');
+    //   // }
+    //   console.log('DId Mount');
     // }
 
-    componentWillReceiveProps(nextProps) {
-      const thisUser = this.props.data.user;
-      console.log('thisUser: ', thisUser);
-      const thisLoading = this.props.data.loading;
-      console.log('thisLoading: ', thisLoading);
+    // componentWillReceiveProps(nextProps) {
+    //   const { user, loading } = nextProps.data;
+    //   console.log('nextProps.data: ', nextProps.data);
+    //   if (!user && !loading) {
+    //     this.props.history.push('/signin');
+    //   }
+    // }
 
-      const nextUser = nextProps.data.user;
-      console.log('nextUser: ', nextUser);
-      const nextLoading = nextProps.data.loading;
-      console.log('nextLoading: ', nextLoading);
-
-      // if (nextProps.data.user !== this.props.user) {
-      //   if (!nextProps.data.user) {
-      //     this.props.history.push('/signin');
-      //   }
-      // }
+    componentWillUpdate(nextProps) {
+      const { user, loading } = nextProps.data;
+      console.log('nextProps.data: ', nextProps.data);
+      if (!user && !loading) {
+        this.props.history.push('/signin');
+      }
     }
 
     render() {
-      // console.log('this.props: ', this.props);
-      // if (!data.user) {
-      //   this.props.history.push('/signin');
-      // }
-
       return <WrappedComponent {...this.props} />;
     }
   }
