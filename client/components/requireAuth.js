44636c944c8a870a53407ea7e794ea9676665ca9
Sean Hong <iamseanhong@gmail.com>
2018-06-30 13:03:54 -0700

    Finish auth-graphql-course

--- client/components/requireAuth.js
+++ client/components/requireAuth.js
@@ -6,22 +6,6 @@ import query_current_user from '../queries/query_current_user';
 
 export default WrappedComponent => {
   class RequireAuth extends Component {
-    // componentDidMount() {
-    //   // const { user, loading } = this.props.data;
-    //   // if (!user && !loading) {
-    //   //   this.props.history.push('/signin');
-    //   // }
-    //   console.log('DId Mount');
-    // }
-
-    // componentWillReceiveProps(nextProps) {
-    //   const { user, loading } = nextProps.data;
-    //   console.log('nextProps.data: ', nextProps.data);
-    //   if (!user && !loading) {
-    //     this.props.history.push('/signin');
-    //   }
-    // }
-
     componentWillUpdate(nextProps) {
       const { user, loading } = nextProps.data;
       if (!user && !loading) {
